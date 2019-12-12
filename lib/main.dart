20d35be7299e64bb62b054798fca79ee74755faf
Bendou Yacine <yacinebendou@gmail.com>
2019-12-12 16:13:50 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // root of the app
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Travel UI',
+      theme: ThemeData(
+          primaryColor: Color(0xFF3EBACE),
+          accentColor: Color(0xFFD8ECF1),
+          scaffoldBackgroundColor: Color(0xFFF3F5F7)),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/destination_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:travel/models/destination_model.dart';
+
+
+class DestinationScreen extends StatefulWidget{
+  final Destination destination;
+  DestinationScreen({this.destination});
+
+
+  _DestinationScreenState createState() =>_DestinationScreenState();
+
+}
+
+class _DestinationScreenState extends State<DestinationScreen>{
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
